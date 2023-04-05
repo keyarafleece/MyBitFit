86e2ae7dcf2acd41a724dcbde16cf5ff964ecab9
keyar <keyarafleece@gmail.com>
2023-04-05 19:36:26 -0400

    BitFit 2.0 attempt. Not working yet.

---
+++ app/src/main/java/com/example/mybitfit/DashboardFragment.kt
@@ -0,0 +1,29 @@
+package com.example.mybitfit
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+
+class DashboardFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.bottom_nav_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    }
--- app/src/main/java/com/example/mybitfit/MainActivity.kt
+++ app/src/main/java/com/example/mybitfit/MainActivity.kt
@@ -4,19 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mybitfit.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     // private variable to inflate the layout for the activity
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     // variable to access the ViewModel class
     val viewModel : ContactViewModel  by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        bottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+       /* val navController = findNavController(R.id.nav_host_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+*/
         // inflate the layout
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
